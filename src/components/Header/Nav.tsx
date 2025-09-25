7550000af4b6bf3b1c6f9e8ba2ffaec96ecacd8a
Fahad_Milhem <fahid2004@gmail.com>
2025-09-25 11:56:08 +0400

    Improvements
    improved About button design

--- src/components/Header/Nav.tsx
+++ src/components/Header/Nav.tsx
@@ -3,10 +3,8 @@ import React from 'react';
 const Nav = () => {
   return (
     <nav className="flex items-center mr-4">
-      <button className="px-6 py-3 text-black text-lg font-thin hover:text-white hover:bg-orange-600 rounded-md transition-all duration-200 group">
-        <span className="group-hover:drop-shadow-[0_0_10px_rgba(255,255,255,0.8)]">
-          About
-        </span>
+      <button className="px-6 py-3 text-black text-lg font-thin hover:text-white hover:scale-110 rounded-md transition-all duration-200">
+        About
       </button>
     </nav>
   );
